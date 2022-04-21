ee097ee6e968611c98c31306cac7d75808825244
Prateek <prateekt91@gmail.com>
2022-04-21 09:20:47 +0530

    [InsertionSort]

--- src/main/java/com/driver/DriverClass.java
+++ src/main/java/com/driver/DriverClass.java
@@ -1,5 +1,6 @@
 package com.driver;
 
+import com.sorting.InsertionSort;
 import com.sorting.SelectionSort;
 
 
@@ -13,10 +14,10 @@ public class DriverClass {
         int[] array = {20,35,-15,7,55,1,-22};
 
       //  BubbleSort bubbleSort = new BubbleSort();
-        SelectionSort selectionSort = new SelectionSort();
+      //  SelectionSort selectionSort = new SelectionSort();
         //int [] sortedArray = bubbleSort.sort(array);
-
-        int [] sortedArray = selectionSort.sort(array);
+        InsertionSort insertionSort = new InsertionSort();
+        int [] sortedArray = insertionSort.sort(array);
         for(int i = 0; i< sortedArray.length ; i++) System.out.print(sortedArray[i] + " ");
 
 
---
+++ src/main/java/com/sorting/InsertionSort.java
@@ -0,0 +1,21 @@
+package com.sorting;
+
+/**
+ * @author Prateek
+ */
+public class InsertionSort {
+
+    public int[] sort(int[] array) {
+
+        for(int firstUnsortedIndex = 1; firstUnsortedIndex < array.length; firstUnsortedIndex ++) {
+            int newElement = array[firstUnsortedIndex];
+
+            int i;
+            for(i = firstUnsortedIndex; i>0 && array[i-1] > newElement; i --) {
+                array[i] = array[i-1];
+            }
+            array[i] = newElement;
+        }
+        return array;
+    }
+}
